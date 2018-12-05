fb69b3748aa73e4717c94c8e0da851a539c7dfa6
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-12-05 16:28:41 -0500

    Removed skip_on_cran from internet-accessing test

--- tests/testthat/testfz_normal.R
+++ tests/testthat/testfz_normal.R
@@ -2,8 +2,6 @@ context("FindZebra API client")
 library("webddx")
 
 test_that("query_fz works for a simple query", {
-  skip_on_cran()
-  
   NUM_ROWS <- 5
   FIELDS <- c("display_title", "retrieved_date", "source", "score")
   tester_df <- query_fz(c("muscle pain", "fatigue"),
